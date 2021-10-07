f9e29ce42d2b076db6e6e0dd86af878c35d9de2f
DeHoKo <zenkovsk@gmail.com>
2021-10-08 01:00:34 +0300

    feat: implement task 5.1

---
+++ import-service/serverless.ts
@@ -0,0 +1,55 @@
+import type { AWS } from '@serverless/typescript';
+
+import importProductsFile from '@functions/importProductsFile';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: ['arn:aws:s3:::${self:provider.environment.SAVED_AND_PARSED_CSV_BUCKET_NAME}']
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: ['arn:aws:s3:::${self:provider.environment.SAVED_AND_PARSED_CSV_BUCKET_NAME}/*']
+      }
+    ],
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      SAVED_AND_PARSED_CSV_BUCKET_NAME: 'rs-school-task5-dehoko-bucket',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { importProductsFile },
+  resources: {
+    Resources: {
+      savedAndParsedCSV: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: '${self:provider.environment.SAVED_AND_PARSED_CSV_BUCKET_NAME}',
+        }
+      }
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
